802dfbc36a273f5d98137535d7f89fbdb977b042
naveensainieso <naveen.saini@eso.sa.gov.au>
2026-05-06 09:41:29 +0930

    merge

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
     base: "/Alert-SA-Web-User-Widget/",
+    build:{
+      
+  rollupOptions: {
+      output: {
+        entryFileNames: 'user-menu.js',
+        assetFileNames: 'user-menu.css'
+      }
+    }
+
+    },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
